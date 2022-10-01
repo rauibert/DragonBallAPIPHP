74db490abeb1df3a4e4d6cfd92c87ade23c3dc15
rauibert <rauibertnf@gmail.com>
2022-10-02 00:52:09 +0200

    Uso de primera plantilla en twig

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 require_once './vendor/autoload.php';
 
-$loader = new \Twig\Loader\ArrayLoader([
-    'index' => 'Hola {{ name }}!',
-]);
-$twig = new \Twig\Environment($loader);
+$loader = new \Twig\Loader\FilesystemLoader('./views');
 
-echo $twig->render('index', ['name' => 'Mundo']);
+$twig = new \Twig\Environment($loader, []);
+
+echo $twig->render('index.twig', ['name' => 'Mundo']);
