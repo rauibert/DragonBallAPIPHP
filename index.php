858d32ea60c84a74e41659866d1f44f0d42e516a
Rauibert <rauibertnf@gmail.com>
2022-12-11 20:08:47 +0100

    Conexion entre menu y personajes

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ $loader = new \Twig\Loader\FilesystemLoader('./views');
 
 $twig = new \Twig\Environment($loader, []);
 
-$data = DataController::loadData();
-$character = DataController::loadCharacter(4);
-
-//echo $twig->render('index.twig', compact('data'));
-echo $twig->render('detalle.twig', compact('character'));
+if(!isset($_GET['id'])){
+  $data = DataController::loadData();
+  echo $twig->render('index.twig', compact('data'));
+}else{
+  $character = DataController::loadCharacter($_GET['id']);
+  echo $twig->render('detalle.twig', compact('character'));
+}
